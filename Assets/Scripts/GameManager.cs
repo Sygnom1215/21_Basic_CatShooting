4a7b3211d9e4dbc38dbb94aa7b3745f2d1492b6a
Sygnom1215 <peacepark112@gmail.com>
2021-06-02 01:57:44 +0900

    애니메이션 속도 수정, 불렛 복사, 적 복사만 만들고 UI 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
-
     public Vector2 minPosition { get; private set; }
     public Vector2 maxPosition { get; private set; }
+
+    [SerializeField]
+    private GameObject enemyPrefab = null;
+    [SerializeField]
+    private Text lifeText = null;
+    [SerializeField]
+    private Text scoreText = null;
+    [SerializeField]
+    private Text highScoreText = null;
+    [SerializeField]
+    private int life = 5;
+    [SerializeField]
+    private int highScore = 0;
+    [SerializeField]
+    private int score = 0;
+
     void Start()
     {
-        minPosition = new Vector2(-7f, -12f);
-        maxPosition = new Vector2(7f, 12f);
+        minPosition = new Vector2(-8f, -4f);
+        maxPosition = new Vector2(8f, 4f);
+        StartCoroutine(SpawnBlackCat());
     }
-
-    void Update()
+    private IEnumerator SpawnBlackCat()
     {
-        
+        float delay = 0f;
+        float positionY = 0f;
+        while (true)
+        {
+            delay = Random.Range(0.3f, 0.7f);
+            positionY = Random.Range(minPosition.y, maxPosition.y);
+            for (int i = 0; i < 5; i++)
+            {
+                Instantiate(enemyPrefab, new Vector2(5f, positionY), Quaternion.identity);
+                yield return new WaitForSeconds(0.2f);
+            }
+            yield return new WaitForSeconds(delay);
+        }
     }
+    private void UpdateUI()
+    {
+        lifeText.text = string.Format("LIFE {0}", life);
+        scoreText.text = string.Format("SCORE {0}", score);
+    //    highScoreText.text = string.Format("BEST {0}", highScore);
+    }
+    //public void AddScore(int addscore)
+    //{
+    //    score += addscore;
+    //    if (score > highScore)
+    //    {
+    //        highScore = score;
+    //        PlayerPrefs.SetInt("HighScore", highScore);
+    //    }
+    //    UpdateUI();
+    //}
+    //public void Dead()
+    //{
+    //    life--;
+    //    if (life <= 0)
+    //    {
+    //        SceneManager.LoadScene("GameOver");
+    //    }
+    //    UpdateUI();
+    //}
+
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField]
+    private GameObject bulletPrefab = null;
+    [SerializeField]
+    private Transform bulletPosition = null;
+    [SerializeField]
+    private float bulletDelay = 0.5f;
+
+    private bool isDamaged = false;
+
+    [SerializeField]
     private float speed = 4f;
 
     private Vector2 targetPosition = Vector2.zero;
@@ -14,6 +23,7 @@ public class PlayerMove : MonoBehaviour
     {
         gameManager = FindObjectOfType<GameManager>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(Curu());
     }
 
     void Update()
@@ -26,4 +36,35 @@ public class PlayerMove : MonoBehaviour
             transform.localPosition = Vector2.MoveTowards(transform.localPosition, targetPosition, speed * Time.deltaTime);
         }
     }
+
+    private IEnumerator Curu()
+    {
+        GameObject bullet;
+        while (true)
+        {
+            bullet = Instantiate(bulletPrefab, bulletPosition);
+            bullet.transform.SetParent(null);
+            yield return new WaitForSeconds(bulletDelay);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isDamaged) return;
+        isDamaged = true;
+    //    StartCoroutine(Dead());
+    }
+
+    //private IEnumerator Dead()
+    //{
+    //    gameManager.Dead();
+    //    isDamaged = true;
+    //    for (int i = 0; i < 5; i++)
+    //    {
+    //        spriteRenderer.enabled = false;
+    //        yield return new WaitForSeconds(0.1f);
+    //        spriteRenderer.enabled = true;
+    //        yield return new WaitForSeconds(0.1f);
+    //    }
+    //    isDamaged = false;
+    //}
 }
